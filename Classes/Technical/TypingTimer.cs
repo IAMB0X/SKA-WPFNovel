46dd222a0644715f17baeacb2ce50cc311bc54d1
Sergat 777 <sergey.kond777@gmail.com>
2024-01-02 09:38:58 +0300

    added text formating bold, italic, color, font size

--- Classes/Technical/TypingTimer.cs
+++ Classes/Technical/TypingTimer.cs
@@ -6,6 +6,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace SKA_Novel.Classes.Technical
@@ -20,6 +23,10 @@ namespace SKA_Novel.Classes.Technical
         private int _letterIndex = 0;
         private int _textLength;
         private TextBlock _targetTextBlock;
+        private Color _foreground;
+        private bool _isBold;
+        private bool _isItalic;
+        private int _fontSize;
         private string _content;
         
         public TypingTimer(TextBlock textBlock, string text)
@@ -29,12 +36,63 @@ namespace SKA_Novel.Classes.Technical
             _textLength = text.Length;
             _content = text;
             _targetTextBlock.Text = "";
+            
+            _isBold = textBlock.FontWeight != FontWeights.Regular;
+            _isItalic = textBlock.FontStyle != FontStyles.Normal;
+            _fontSize = (int)textBlock.FontSize;
+            _foreground = ((SolidColorBrush)textBlock.Foreground).Color;
+
             Timer.Tick += TypingText;
             Timer.Start();
             if (ControlsManager.TypingTimer != null)
                 ControlsManager.TypingTimer.Timer.Stop();
             ControlsManager.TypingTimer = this;
         }
+
+        private void FormatText()
+        {
+            if (_content[_letterIndex] == '#')
+            {
+                _foreground = (Color)ColorConverter.
+                    ConvertFromString(_content.Substring(_letterIndex, 7));
+
+                _letterIndex += 7;
+                return;
+            }
+
+            if (_content[_letterIndex] == 'f')
+            {
+                _fontSize = Convert.ToInt16(_content.Substring(_letterIndex + 1, 2));
+                _letterIndex += 3;
+                return;
+            }
+
+            if (_content[_letterIndex] == 'b')
+                _isBold = true;
+
+            if (_content[_letterIndex] == 'i')
+                _isItalic = true;
+
+            _letterIndex++;
+        }
+
+        private void UnformatText()
+        {
+            if (_content[_letterIndex] == '#')
+                _foreground = ((SolidColorBrush)(_targetTextBlock.Foreground)).Color;
+
+            if (_content[_letterIndex] == 'b')
+                _isBold = false;
+
+            if (_content[_letterIndex] == 'i')
+                _isItalic = false;
+
+            if (_content[_letterIndex] == 'f')
+                _fontSize = (int)_targetTextBlock.FontSize;
+
+            _letterIndex++;
+        }
+
 
         public void TypingText(object sender, EventArgs e)
         {
@@ -44,10 +102,37 @@ namespace SKA_Novel.Classes.Technical
                 {
                     _targetTextBlock.Text += "\n";
                     _letterIndex++;
+
+                    _fontSize = 30;
+                    _isBold = _targetTextBlock.FontWeight != FontWeights.Regular;
+                    _isItalic = _targetTextBlock.FontStyle != FontStyles.Normal;
+                    _foreground = Colors.White;
                 }
                 else
                 {
-                    _targetTextBlock.Text += _content[_letterIndex];
+                    while (_content[_letterIndex] == '>' || _content[_letterIndex] == '<')
+                    {
+                        _letterIndex++;
+
+                        if (_content[_letterIndex - 1] == '>')
+                            FormatText();
+
+                        if (_content[_letterIndex - 1] == '<')
+                            UnformatText();
+                    }
+
+                    Run letter = new Run(_content[_letterIndex].ToString())
+                    {
+                        FontSize = _fontSize,
+                        Foreground = new SolidColorBrush(_foreground)
+                    };
+
+                    if (_isBold)
+                        letter.FontWeight = FontWeights.Bold;
+                    if (_isItalic)
+                        letter.FontStyle = FontStyles.Italic;
+
+                    _targetTextBlock.Inlines.Add(letter);
                     _letterIndex++;
                 }
             }
